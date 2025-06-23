e5cbd488e43986277a8189b0ea3790d931ac187d
MichelleGel <michellegelves@gmail.com>
2025-06-23 23:14:40 +0200

    improving navegation bar

--- src/js/donde-estoy.js
+++ src/js/donde-estoy.js
@@ -1,1 +1,44 @@
 /*hola*/
+/*NAV*/
+
+/*titulo LITIO*/
+/*
+import { animate } from "animejs";
+
+animate("span", {
+    // Property keyframes
+    y: [
+        { to: "-2.75rem", ease: "outExpo", duration: 600 },
+        { to: 0, ease: "outBounce", duration: 800, delay: 100 },
+    ],
+    // Property specific parameters
+    rotate: {
+        from: "-1turn",
+        delay: 0,
+    },
+    delay: (_, i) => i * 50, // Function based value
+    ease: "inOutCirc",
+    loopDelay: 1000,
+    loop: true,
+});
+*/
+/*const links = document.querySelectorAll(".litio-navbar a");
+links.forEach((link) => {
+    if (link.href === window.location.href) {
+        link.classList.add("activo");
+    }
+});*/
+/*
+const toggleBtn = document.getElementById("toggleNav");
+const barra = document.getElementById("barraLateral");
+
+toggleBtn.addEventListener("click", () => {
+    barra.classList.toggle("mostrar");
+});
+
+// Cierra menú al hacer clic en cualquier enlace (opcional)
+document.querySelectorAll(".barra-lateral a").forEach((link) => {
+    link.addEventListener("click", () => {
+        barra.classList.remove("mostrar");
+    });
+});*/
