98a62f9bea6e09eddba17adda29c6dd4f4b1a2de
ingridD2707 <im938407@gmail.com>
2025-06-23 03:31:30 -0700

    index-page

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,53 @@
-/*hola*/
+ 
+
+
+
+particlesJS("particles-js", {
+  "particles": {
+    "number": {
+      "value": 80,
+      "density": {
+        "enable": true,
+        "value_area": 800
+      }
+    },
+    "color": { "value": "#ffffff" },
+    "shape": {
+      "type": "circle",
+      "stroke": { "width": 0, "color": "#000000" }
+    },
+    "opacity": {
+      "value": 0.5,
+      "random": false
+    },
+    "size": {
+      "value": 3,
+      "random": true
+    },
+    "move": {
+      "enable": true,
+      "speed": 2,
+      "direction": "none",
+      "out_mode": "out"
+    }
+  },
+  "interactivity": {
+    "events": {
+      "onhover": { "enable": true, "mode": "repulse" },
+      "onclick": { "enable": true, "mode": "push" }
+    },
+    "modes": {
+      "repulse": { "distance": 100 },
+      "push": { "particles_nb": 4 }
+    }
+  },
+  "retina_detect": true
+});
+  document.getElementById("enter-button").addEventListener("click", function () {
+  document.getElementById("intro-screen").style.display = "none";
+  document.getElementById("main-content").style.display = "block";
+});
+  document.getElementById("back-button").addEventListener("click", function () {
+  document.getElementById("main-content").style.display = "none";
+  document.getElementById("intro-screen").style.display = "flex";
+});
